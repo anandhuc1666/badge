7f61beedd767b9a8af2b09c8297e07026359da78
anandhuc1666 <anandhuc166@gmail.com>
2025-09-20 12:06:40 +0530

    NewTodo

--- src/App.jsx
+++ src/App.jsx
@@ -44,45 +44,84 @@
 // }
 
 // export default App
-import React from 'react'
-import { useState } from 'react'
+// import React from 'react'
+// import { useState } from 'react'
+
+// function App() {
+//   const [text, setText] = useState('')
+//   const [todo, setTodo] = useState([])
+//   const [editIndex, setEditIndex] = useState(null) 
+//   const [textedit, setTextedit] = useState('')
+//   const handleSend =()=>{
+//     setTodo([...todo,text])
+//     setText("")
+//   }
+//   const handleEdit = (index) =>{
+//      setEditIndex(index)
+//      setTextedit(todo[index])
+//   }
+//   const handleUpdate =(index)=>{
+//     setTodo([...todo,textedit])
+//     setEditIndex(null)
+//     setTodo((pre)=>pre.filter((i,k)=> !k == index) )
+//   }
+//   return (
+//     <div>
+//     <input type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
+//     <button onClick={handleSend}>send</button>
+
+//     <ul>
+//       {
+//         todo.map((i,k)=>(
+//           <li key={k}>
+//           {
+//            editIndex === k ? <><input type="text" value={textedit} onChange={(e)=>setTextedit(e.target.value)}/>
+//             <button onClick={()=>handleUpdate(k)}>update</button></>
+//            :<>{i} <button onClick={()=>handleEdit(k)}>edit</button></>
+//           }
+//           </li>
+//         ))
+//       }
+//     </ul>
+//     </div>
+//   )
+// }
+
+// export default App
+import React, { useState } from 'react'
 
 function App() {
-  const [text, setText] = useState('')
-  const [todo, setTodo] = useState([])
-  const [editIndex, setEditIndex] = useState(null) 
-  const [textedit, setTextedit] = useState('')
-  const handleSend =()=>{
+  const[text,setText]=useState('')
+  const[todo,setTodo]=useState([])
+  const[edit,setEdit]=useState(null)
+  const[textEdit,setTextEdit]=useState('')
+  const handleSend=()=>{
     setTodo([...todo,text])
     setText("")
   }
-  const handleEdit = (index) =>{
-     setEditIndex(index)
-     setTextedit(todo[index])
-  }
-  const handleUpdate =(index)=>{
-    setTodo([...todo,textedit])
-    setEditIndex(null)
-    setTodo((pre)=>pre.filter((i,k)=> !k == index) )
+  const handleEdit =(index)=>{
+     setEdit(index)
+     setTextEdit(todo[index])
   }
   return (
     <div>
-    <input type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
-    <button onClick={handleSend}>send</button>
-
-    <ul>
-      {
-        todo.map((i,k)=>(
-          <li key={k}>
-          {
-           editIndex === k ? <><input type="text" value={textedit} onChange={(e)=>setTextedit(e.target.value)}/>
-            <button onClick={()=>handleUpdate(k)}>update</button></>
-           :<>{i} <button onClick={()=>handleEdit(k)}>edit</button></>
-          }
-          </li>
-        ))
-      }
-    </ul>
+      <input type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
+      <button onClick={handleSend}>send</button>
+      <ul>
+        {
+          todo.map((i,k)=>(
+            <li key={k}>
+                  {
+                    edit === k ? 
+                    <>
+                    <input type="text" value={textEdit} onChange={(e)=>setTextEdit(e.target.value)}/>
+                    <button onClick={()=>handleEdit(k)}>edit</button>
+                    </>:<></>
+                  }
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
